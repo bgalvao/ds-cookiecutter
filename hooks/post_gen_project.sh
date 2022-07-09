11b63a58ade0d875c0015d69108039b4ffb5c1d4
bgalvao <brnrd.glv@gmail.com>
2022-07-09 10:01:36 +0100

    stop tracking models

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,7 +1,9 @@
-mamba install \
-  --file "{{ cookiecutter.project_name }}/environment.txt" \
-  -n "{{ cookiecutter.env_name }}"
-
-mamba install -n "{{ cookiecutter.env_name }}" pip -y
+#!/usr/bin/bash
+set -e
+mamba create -n "{{ cookiecutter.env_name }}"
 mamba activate "{{ cookiecutter.env_name }}"
+mamba install --file "environment.txt"
+mamba install pip -y
 pip install "prefect>=2.0b" mlem
+
+echo "Enjoy your MLOps-enabled data science project! 🍜"
